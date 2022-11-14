13e100ebd9eb663b026965fe877c317acd5d5a3a
Bellantra <yamilapaez2@hotmail.com>
2022-11-14 18:46:01 +0100

    theme changes fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { globalTheme } from './themes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+		<ThemeProvider theme={globalTheme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
 );
