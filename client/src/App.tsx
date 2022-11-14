eb444f332795ea501d8eba4c429042da494e676b
JoarDev <j.ramos5452@gmail.com>
2022-11-14 13:52:35 -0300

    carousel component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import Home from './pages/home/Home';
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <div className='App'></div>;
+	return (
+    <Home/>
+  );
 }
 
 export default App;
