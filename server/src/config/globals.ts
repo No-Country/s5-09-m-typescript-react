07dbf6d315f352e8c00f2caa365b5e697baaedb5
Emanuel Palacio <97160610+EmanuelPalacio@users.noreply.github.com>
2022-11-16 08:18:29 -0300

    Revert "Francisco/boletin informativo"

--- server/src/config/globals.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export const dbTest: string = process.env.DB_TESTING || ''
-export const dbDev: string = process.env.DB_DEVELOPMENT || ''
-export const porTest = process.env.PORT_TESTING || ''
-export const portDev = process.env.PORT_DEVELOPMENT || ''
