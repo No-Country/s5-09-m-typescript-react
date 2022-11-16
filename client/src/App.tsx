656a7b4aad7d3e1ba56c004af599df92796c6c8b
Federico Di Cillo <dicillo.fe@gmail.com>
2022-11-16 12:35:14 -0300

    pruebas contact form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
 
 	return (
 		<>
-			<MenuNavegation></MenuNavegation>
-			
-			<Contact/>
-			<Footer></Footer>
+		{/* <InputLogin /> */}
+			<MenuNavegation />
+			<Home />
+			<Footer />
 		</>
 	);
 }
