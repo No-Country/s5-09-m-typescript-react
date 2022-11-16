b02aaa11325e021d8b90353fd794733802fcc4d7
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-11-16 11:07:08 -0300

    layout fixes

--- client/src/pages/home/components/index.ts
+++ client/src/pages/home/components/index.ts
@@ -2,4 +2,4 @@ export * from './Hero';
 export { default as MyPractices } from './MyPractices';
 export { default as Slider } from './Slider';
 export { default as Testimonials } from './Testimonials';
-export { default as BoletinInformativo } from './BoletinInformativo';
+export { default as Newsletter } from './Newsletter';
