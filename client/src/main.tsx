492b5262038c3146244da6e7b0bab3ec5fe07a6a
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-11-15 22:05:27 -0300

    add configuration Mui Theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { globalTheme } from './themes';
+import Theme from './themes/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ThemeProvider theme={globalTheme}>
+		<Theme>
 			<App />
-		</ThemeProvider>
+		</Theme>
 	</React.StrictMode>,
 );
