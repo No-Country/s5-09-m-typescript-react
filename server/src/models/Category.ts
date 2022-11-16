2ffc5fe4da54d024d28a809e2b31df49a2cc6fa4
astradae <astradaezequiel.tpi@gmail.com>
2022-11-16 16:29:35 -0300

    modelo category creado

---
+++ server/src/models/Category.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose'
+import { Category } from '../interfaces/category'
+
+const categorySchema = new Schema<Category>({
+    name: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+        default:
+            'https://www.softzone.es/app/uploads-softzone.es/2018/04/guest.png',
+    },
+    description: {
+        type: String,
+        required: true,
+    }
+})
+
+const Category = model('Habit', categorySchema)
+export default Category
