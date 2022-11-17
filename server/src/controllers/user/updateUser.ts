a10e8fbf57905e22e3cd27778a268d8e056c462c
Maximiliano Miño <libertadores323@gmail.com>
2022-11-17 03:54:56 -0300

    Maxi/user-crud

---
+++ server/src/controllers/user/updateUser.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import * as userService from '../../services/users'
+
+export const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { body } = req
+
+    try {
+        const userRetrieved: any = await userService.updateUser(id, body)
+        const { status } = userRetrieved
+        return res.status(status).json({
+            userRetrieved,
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error,
+        })
+    }
+}
