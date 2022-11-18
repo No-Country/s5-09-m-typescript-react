1d5c89993f5a7fffa5432a402a5a45c30ea7b656
astradae <astradaezequiel.tpi@gmail.com>
2022-11-18 13:57:26 -0300

    routes index - agregado rutas: /habit, /category

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import emailRoutes from './emailingRoutes'
 import userRoutes from './userRoutes'
 import authRoutes from './authRoutes'
+import habitsRoutes from './habitRoutes'
+import categoryRoutes from './categoryRoutes'
 
 const router = express.Router()
 
 router.use('/', emailRoutes)
 router.use('/user', userRoutes)
 router.use('/', authRoutes)
+router.use('/habit', habitsRoutes)
+router.use('/category', categoryRoutes)
 
 export { router }
