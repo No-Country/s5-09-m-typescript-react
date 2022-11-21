c54780f8ab8384a2db41e2ec97f8874781419113
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-11-21 11:28:36 -0500

    facebook login in process

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,28 +7,27 @@ import Login from './pages/login/Login';
 import AboutUs from './pages/aboutus/AboutUs';
 import Practices from './pages/practices/Practices';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { FacebookProvider } from 'react-facebook';
 
 function App() {
 	return (
-		<GoogleOAuthProvider
-			clientId={
-				'460211922745-6t4k8cs9kk2a48a6c33r20g6mjel9tfu.apps.googleusercontent.com'
-			}
-		>
-			<BrowserRouter>
-				<LayoutNavegation>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/misPracticas' element={<Practices />} />
-						<Route path='/contacto' element={<Contact />} />
-						<Route path='/iniciarSesion' element={<Login />} />
-						<Route path='/nostros' element={<AboutUs />} />
-						<Route path='/registrate' element={<Signup />} />
-						<Route path='/nosotros' element={<h1>en contruccion</h1>} />
-					</Routes>
-				</LayoutNavegation>
-			</BrowserRouter>
-		</GoogleOAuthProvider>
+		<FacebookProvider appId={'461951009397297'}>
+			<GoogleOAuthProvider clientId={import.meta.env.VITE_APP_GOOGLE_ID}>
+				<BrowserRouter>
+					<LayoutNavegation>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/misPracticas' element={<Practices />} />
+							<Route path='/contacto' element={<Contact />} />
+							<Route path='/iniciarSesion' element={<Login />} />
+							<Route path='/nostros' element={<AboutUs />} />
+							<Route path='/registrate' element={<Signup />} />
+							<Route path='/nosotros' element={<h1>en contruccion</h1>} />
+						</Routes>
+					</LayoutNavegation>
+				</BrowserRouter>
+			</GoogleOAuthProvider>
+		</FacebookProvider>
 	);
 }
 
