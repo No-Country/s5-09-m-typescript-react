661e8fb61f8622d526c0cd8671c0c8826e298b19
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-11-20 22:20:54 -0300

    redux install

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,19 @@
-import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import Theme from './themes/Theme';
 import { ProSidebarProvider } from 'react-pro-sidebar';
+//redux
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ProSidebarProvider>
 			<Theme>
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 			</Theme>
 		</ProSidebarProvider>
 	</React.StrictMode>,
