8d430ecf7d1e2a1ac9ede2892eb40219697b879b
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-11-22 09:28:48 -0500

    input validations implemented

--- client/src/pages/login/components/InputLogin.tsx
+++ client/src/pages/login/components/InputLogin.tsx
@@ -6,6 +6,8 @@ import {
 	Typography,
 	Checkbox,
 	FormControlLabel,
+	IconButton,
+	InputAdornment,
 } from '@mui/material';
 
 import { Link as RouterLink } from 'react-router-dom';
@@ -14,7 +16,10 @@ import { useForm } from 'react-hook-form';
 import { useGoogleLogin } from '@react-oauth/google';
 import { onLogin, onLoginGoogle } from '../../../service/authApi';
 import axios from 'axios';
-import { FacebookProvider, useLogin } from 'react-facebook';
+// import { LoginSocialFacebook } from 'reactjs-social-login';
+import { isEmail } from '../../../utilities';
+import { Message, Visibility, VisibilityOff } from '@mui/icons-material';
+import { useState } from 'react';
 
 type Inputs = {
 	email: string;
@@ -23,6 +28,7 @@ type Inputs = {
 };
 
 export default function InputLogin() {
+	const [showPassword, setshowPassword] = useState(false);
 	const {
 		register,
 		handleSubmit,
@@ -48,19 +54,17 @@ export default function InputLogin() {
 		},
 	});
 
-	const { login: loginFacebook, status, isLoading, error } = useLogin();
+	// async function handleLogin() {
+	// 	try {
+	// 		const response = await loginFacebook({
+	// 			scope: 'email',
+	// 		});
 
-	async function handleLogin() {
-		try {
-			const response = await loginFacebook({
-				scope: 'email',
-			});
-
-			console.log(response.status);
-		} catch (error: any) {
-			console.log(error.message);
-		}
-	}
+	// 		console.log(response.status);
+	// 	} catch (error: any) {
+	// 		console.log(error.message);
+	// 	}
+	// }
 
 	return (
 		<form onSubmit={handleSubmit(onLogin)}>
@@ -75,6 +79,7 @@ export default function InputLogin() {
 					<TextField
 						{...register('email', {
 							required: 'Este campo es requerido',
+							validate: isEmail,
 						})}
 						error={!!errors.email}
 						helperText={errors.email?.message}
@@ -89,15 +94,27 @@ export default function InputLogin() {
 					<TextField
 						{...register('password', {
 							required: 'Este campo es requerido',
-							minLength: { value: 6, message: 'Mínimo 6 caracteres' },
 						})}
 						error={!!errors.password}
 						helperText={errors.password?.message}
 						label='Contraseña'
-						type='password'
 						variant='outlined'
 						color='text'
 						fullWidth
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position='start'>
+									<IconButton
+										aria-label='toggle password visibility'
+										onClick={() => setshowPassword((prev: any) => !prev)}
+										edge='end'
+									>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+						type={showPassword ? 'text' : 'password'}
 					/>
 				</Grid>
 				<Grid item xs={6}>
@@ -150,18 +167,28 @@ export default function InputLogin() {
 						alignItems: 'center',
 					}}
 				>
-					<Button
-						variant='outlined'
-						fullWidth
-						sx={{ maxWidth: '100px' }}
-						onClick={handleLogin}
-						disabled={isLoading}
+					{/* <LoginSocialFacebook
+						appId='461951009397297'
+						onResolve={res => {
+							console.log(res);
+						}}
+						onReject={error => {
+							console.log(error);
+						}}
 					>
-						<img
-							src='https://res.cloudinary.com/dlxlitkl6/image/upload/v1668694018/ananda%20marga/facebook_ic_ashpl3.svg'
-							alt='132'
-						/>
-					</Button>
+						<Button
+							variant='outlined'
+							fullWidth
+							sx={{ maxWidth: '100px' }}
+							// onClick={handleLogin}
+							// disabled={isLoading}
+						>
+							<img
+								src='https://res.cloudinary.com/dlxlitkl6/image/upload/v1668694018/ananda%20marga/facebook_ic_ashpl3.svg'
+								alt='132'
+							/>
+						</Button>
+					</LoginSocialFacebook> */}
 					<Button
 						variant='outlined'
 						fullWidth
