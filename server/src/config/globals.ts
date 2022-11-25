3734e5e6fa881f12f21af628bd213257dcb536f5
Bellantra <yamilapaez2@hotmail.com>
2022-11-25 22:11:54 +0100

    fix dotenv

--- server/src/config/globals.ts
+++ server/src/config/globals.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
 export const dbTest: string = process.env.DB_TESTING || ''
 export const dbDev: string = process.env.DB_DEVELOPMENT || ''
 export const portTest = process.env.PORT_TESTING || ''
