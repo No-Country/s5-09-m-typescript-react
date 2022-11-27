a82d7559582ea88780d4436de2d97e3677a9681b
Bellantra <64668681+Bellantra@users.noreply.github.com>
2022-11-27 21:59:25 +0100

    Revert "Yamila/auth routes"

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,20 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, DELETE'
-    )
-    next()
-})
-
 app.use('/', router)
 
 // * ----------Connection with Mongo Atlas ------------
