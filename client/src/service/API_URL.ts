2d3600445ca5d8d2cf98779d514aeef9d2b113ce
Bellantra <yamilapaez2@hotmail.com>
2022-11-29 21:50:57 +0100

    validations added for authRoutes

--- client/src/service/API_URL.ts
+++ client/src/service/API_URL.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const API_URL = axios.create({
 	baseURL:
-		// process.env.NODE_ENV === 'development'
-		/*usar esta condicion si se quiere testear back deployeado desde el front local*/
-		process.env.NODE_ENV === 'production' ||
 		process.env.NODE_ENV === 'development'
 			? import.meta.env.VITE_APP_BACKEND_URL
 			: import.meta.env.VITE_APP_LOCAL_BACKEND, // no hay rutas /api,
