3f72d4875a78e372fae28dd2eb461cc70f36d332
Bellantra <yamilapaez2@hotmail.com>
2022-11-29 21:56:48 +0100

    fix

--- client/src/service/API_URL.ts
+++ client/src/service/API_URL.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const API_URL = axios.create({
 	baseURL:
-		process.env.NODE_ENV === 'development'
+		process.env.NODE_ENV === 'production'
 			? import.meta.env.VITE_APP_BACKEND_URL
 			: import.meta.env.VITE_APP_LOCAL_BACKEND, // no hay rutas /api,
 });
