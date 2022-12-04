8afcf83d4fad0ff53ffcbffee34c89be0f9d6379
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-12-04 09:01:18 -0300

    modify register habits

--- client/src/pages/signup/Signup.tsx
+++ client/src/pages/signup/Signup.tsx
@@ -1,13 +1,21 @@
-import { LayoutForms } from '../../components';
+import { HabitsModal, LayoutForms } from '../../components';
+import { useAppSelector, useAppDispatch } from '../../redux/hooks';
 import SignupForm from './components/SignupForm';
+import { changeShowHabitModal } from '../../redux/slices/setting';
 
 export default function Signup() {
+	const dispatch = useAppDispatch();
+	const showModal = useAppSelector(state => state.setting.showHabitModal);
 	return (
 		<LayoutForms
 			tittle='Bienvenido'
 			text='Crea tu cuenta  y disfruta de todo lo que tenemos para ti.'
 		>
-			<SignupForm />
+			{!showModal ? (
+				<SignupForm />
+			) : (
+				<HabitsModal closeModal={() => dispatch(changeShowHabitModal())} />
+			)}
 		</LayoutForms>
 	);
 }
