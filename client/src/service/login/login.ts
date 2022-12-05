e5adcea88bf694d4e82590150b1d9b53b3cb6ed3
Federico Di Cillo <dicillo.fe@gmail.com>
2022-12-05 10:37:33 -0300

    Mostrando error de formulario, habia que cambiar el idioma del texto de error

--- client/src/service/login/login.ts
+++ client/src/service/login/login.ts
@@ -43,7 +43,7 @@ export const onLogin = async (
 		} else {
 			if (
 				data.error[0].msg ===
-				'The Password has to contain min 8 chars, 1 lowercase(min), 1 uppercase(min), 1 number(min), 1 symbol(min) '
+				'La contraseña debe contener min 8 carácteres, 1 minúscula(min), 1 mayúscula(min), 1 numero(min), 1 simbolo(min) '
 			) {
 				dispatch(changeErrorPassword());
 			}
